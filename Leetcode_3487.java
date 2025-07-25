b539ca56d59d7f7eba1dd5f15cf71e4e5efec785
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-07-25 22:42:16 +0530

    3487. Maximum Unique Subarray Sum After Deletion

---
+++ Leetcode_3487.java
@@ -0,0 +1,11 @@
+//3487. Maximum Unique Subarray Sum After Deletion
+class Solution {
+    public int maxSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=nums[nums.length-1], prev=sum;
+        for(int i=nums.length-2;i>=0 &&nums[i]>=0;prev=nums[i],i--){
+            if(nums[i]!=prev) sum+=nums[i];         
+        }
+        return sum;
+    }
+}
