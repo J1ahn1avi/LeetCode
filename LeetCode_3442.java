242978a7d25e6a7a11153bc93db8b13f9b81aa74
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-04 09:53:31 +0530

    3442. Maximum Difference Between Even and Odd Frequency I

---
+++ LeetCode_3442.java
@@ -0,0 +1,22 @@
+//3442. Maximum Difference Between Even and Odd Frequency I
+class Solution {
+    public int maxDifference(String s) {
+        HashMap<Character,Integer>dict=new HashMap<>();
+
+        for(char i : s.toCharArray()){
+            dict.put(i,dict.getOrDefault(i,0)+1);
+        }
+
+        int max_odd=0;
+        int min_even=Integer.MAX_VALUE;
+
+        for(int i:dict.values()){
+            if(i%2!=0){
+                max_odd=Math.max(max_odd,i);
+            }else{
+                min_even=Math.min(min_even,i);
+            }
+        }
+        return max_odd-min_even;
+    }
+}
