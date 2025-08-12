a3ebed87e35cc90446d64e0c7df942d9576355e1
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-12 20:31:45 +0530

    1071. Greatest Common Divisor of Strings

---
+++ Leetcode_1071.java
@@ -0,0 +1,15 @@
+//1071. Greatest Common Divisor of Strings
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1))
+            return "";
+        int a = str1.length();
+        int b = str2.length();
+        while (b != 0) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return str1.substring(0, a);
+    }
+}
