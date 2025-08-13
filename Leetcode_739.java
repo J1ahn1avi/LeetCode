23d0bed5d1efe69a6f425f4e32f8a31dc6172f87
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-13 10:52:03 +0530

    739. Daily Temperatures

---
+++ Leetcode_739.java
@@ -0,0 +1,17 @@
+//739. Daily Temperatures
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int res[] = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peek()]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                res[i] = stack.peek() - i;
+            }
+            stack.push(i);
+        }
+        return res;
+    }
+}
