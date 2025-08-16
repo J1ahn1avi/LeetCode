d4e01c27fbaef1f679fb44075d92cdba51b0de1f
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-16 15:51:18 +0530

    367. Valid Perfect Square

---
+++ Leetcode_367.java
@@ -0,0 +1,20 @@
+//367. Valid Perfect Square
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if (num == 1) return true;
+        int low=1;
+        int high=num/2;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((long)mid*mid==num){
+                return true;
+            }else if ((long)mid*mid>num){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return false;
+    }
+}
