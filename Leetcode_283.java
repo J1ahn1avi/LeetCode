ba0dd52f209dcd92cb053e3f74b7fef1fdb0e298
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-18 11:03:01 +0530

    283. Move Zeroes

---
+++ Leetcode_283.java
@@ -0,0 +1,17 @@
+//283.Move Zeroes
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int p1 = 0, p2 = 0;
+        while (p2 < nums.length) {
+            if (nums[p2] != 0) {
+                nums[p1] = nums[p2];
+                p1++;
+            }
+            p2++;
+        }
+        while (p1 < nums.length) {
+            nums[p1] = 0;
+            p1++;
+        }
+    }
+}
