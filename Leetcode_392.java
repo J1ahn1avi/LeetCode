58a78f24bd191cffea9dd8705ad91ec8fae299cb
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-18 10:04:46 +0530

    392. Is Subsequence

---
+++ Leetcode_392.java
@@ -0,0 +1,14 @@
+//392. Is Subsequence
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int left=0;
+        int right=0;
+        while(left<s.length() && right<t.length()){
+            if(s.charAt(left)==t.charAt(right)){
+                left++;
+            }
+            right++;
+        }
+        return left==s.length();
+    }
+}
