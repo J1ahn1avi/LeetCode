a11611a112eb57233ccd9f4674e292dbc9046ee7
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-18 12:15:42 +0530

    Two Sum II - Input Array Is Sorted

--- Leetcode_167.java
+++ Leetcode_167.java
@@ -2,7 +2,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        int res[] = new int[2];
         while (left < right) {
             if (nums[left] + nums[right] == target) {
                 return new int[] { left + 1, right + 1 };
