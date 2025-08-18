1df648a2bf45e319472daf32bcf5a6012aaea92d
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-18 12:09:32 +0530

    167. Two Sum II - Input Array Is Sorted

---
+++ Leetcode_167.java
@@ -0,0 +1,17 @@
+//167. Two Sum II - Input Array Is Sorted
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int res[] = new int[2];
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[] { left + 1, right + 1 };
+            } else if (nums[left] + nums[right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
