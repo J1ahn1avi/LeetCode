d609376caa7e7747d9fd1585162cb60d76eaafa6
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-08-19 20:28:32 +0530

    1207. Unique Number of Occurrences

---
+++ Leetcode_1207.java
@@ -0,0 +1,16 @@
+//1207. Unique Number of Occurrences
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        Set<Integer> res = new HashSet<>();
+        for (int i : map.values()) {
+            if (!res.add(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
