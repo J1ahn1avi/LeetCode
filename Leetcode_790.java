b162dfe3371ef0eaad82003cfde11ddfc811a588
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-09-12 12:04:36 +0530

    790. Domino and Tromino Tiling

---
+++ Leetcode_790.java
@@ -0,0 +1,17 @@
+//790. Domino and Tromino Tiling
+class Solution {
+    public int numTilings(int n) {
+        int mod=1_000_000_007;
+        if(n==1) return 1;
+        if(n==2) return 2;
+
+        long dp[]=new long[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=(2*dp[i-1]+dp[i-3])%mod;
+        }
+        return (int)dp[n];
+    }
+}
