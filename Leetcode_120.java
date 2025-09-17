1d1135d1437891f205e7da6852150152282edbaf
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-09-17 10:07:49 +0530

    120. Triangle

---
+++ Leetcode_120.java
@@ -0,0 +1,14 @@
+//120. Triangle
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int dp[][] = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = triangle.get(i).get(j) + Math.min(dp[i + 1][j + 1], dp[i + 1][j]);
+            }
+        }
+        return dp[0][0];
+    }
+}
