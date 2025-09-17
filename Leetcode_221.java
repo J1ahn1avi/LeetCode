fa371cd5a0c93010df00d25c72186d554479c211
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-09-17 12:16:06 +0530

    221. Maximal Square

---
+++ Leetcode_221.java
@@ -0,0 +1,20 @@
+//221. Maximal Square
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int dp[][]=new int[m+1][n+1];
+        if(matrix==null || m==0 || n==0) return 0;
+
+        int max=0;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(matrix[i-1][j-1]=='1'){
+                    dp[i][j]=Math.min(dp[i][j-1],Math.min(dp[i-1][j],dp[i-1][j-1]))+1;
+                    max=Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        return max*max;
+    }
+}
