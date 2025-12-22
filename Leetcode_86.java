cc136e332c191ebf8934a0d5c53f229ba75b3e22
Nagulla Jahnavi <146811286+J1ahn1avi@users.noreply.github.com>
2025-12-22 10:28:25 +0530

    86. Partition List

---
+++ Leetcode_86.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode p1 = new ListNode(0);
+        ListNode p2 = new ListNode(0);
+        ListNode l1 = p1;
+        ListNode l2 = p2;
+
+        while (head != null) {
+            if (head.val < x) {
+                l1.next = head;
+                l1 = l1.next;
+            } else {
+                l2.next = head;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+
+        l1.next = p2.next;
+        l2.next = null;
+
+        return p1.next;
+    }
+}
